5d2bea5709114a8ecdf171624024c2776ed268a1
alexlevsky <tolevotolev@gmail.com>
2024-08-20 21:58:09 +0300

    add some updates

--- src/components/SelectSexQuestion.tsx
+++ src/components/SelectSexQuestion.tsx
@@ -16,13 +16,16 @@ const SelectSexQuestion: React.FC<SelectSexQuestionProps> = ({ question, onAnswe
       <h2>{question.question}</h2>
       <div className="options">
         {question.options.map(option => (
-          <button
-            key={option}
-            className={`bg-primary text-white px-4 py-2 rounded font-sans ${selectedAnswer === option ? 'selected' : ''}`}
-            onClick={() => onAnswer(option)}
-          >
-            {option}
-          </button>
+          <div className='flex items-center' key={option}>
+            <button 
+              key={option}
+              className={`bg-primary text-white px-4 py-2 rounded font-sans my-1 w-full
+              ${selectedAnswer === option ? 'selected' : ''}`}
+              onClick={() => onAnswer(option)}
+            >
+              {option}
+            </button>
+          </div>
         ))}
       </div>
     </div>
--- src/components/SingleAnswerQuestion.tsx
+++ src/components/SingleAnswerQuestion.tsx
@@ -16,17 +16,25 @@ const SingleAnswerQuestion: React.FC<SingleAnswerQuestionProps> = ({ question, o
       <h2>{question.question}</h2>
       <div className="options">
         {question.options.map(option => (
-          <label key={option}>
-            <input
-              type="radio"
-              name={`question-${question.id}`}
-              value={option}
-              checked={selectedAnswer === option}
-              onChange={() => onAnswer(option)}
-              className='form-checkbox h-5 w-5 text-blue-600 rounded focus:ring-blue-500 font-sans'
-            />
-            {option}
+        <div className='flex items-center  w-full h-15 border-2 border-black mx-0 px-2' key={option}>
+          <label>
+            <div className='flex items-center justify-center'>
+                <div>
+                    <input
+                    type="radio"
+                    name={`question-${question.id}`}
+                    value={option}
+                    checked={selectedAnswer === option}
+                    onChange={() => onAnswer(option)}
+                    className='form-checkbox h-5 w-5 text-blue-600 rounded focus:ring-blue-500 font-sans mx-1'
+                    />
+                </div>
+                <div>
+                    {option}
+                </div>
+            </div>
           </label>
+        </div>
         ))}
       </div>
     </div>
