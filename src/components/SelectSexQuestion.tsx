e9224291e57c303430e375f7b354e69096bd24e9
alexlevsky <tolevotolev@gmail.com>
2024-08-20 19:55:50 +0300

    add SelectSexQuestion component

---
+++ src/components/SelectSexQuestion.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface SelectSexQuestionProps {
+  question: {
+    id: number;
+    question: string;
+    options: string[];
+  };
+  onAnswer: (answer: string) => void;
+  selectedAnswer?: string;
+}
+
+const SelectSexQuestion: React.FC<SelectSexQuestionProps> = ({ question, onAnswer, selectedAnswer }) => {
+  return (
+    <div className="select-sex-question">
+      <h2>{question.question}</h2>
+      <div className="options">
+        {question.options.map(option => (
+          <button
+            key={option}
+            className={`option-button ${selectedAnswer === option ? 'selected' : ''}`}
+            onClick={() => onAnswer(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SelectSexQuestion;
